a921a28c4c5786d2265908b17c201bd7d4f28d41
Austin Horning <austinseven@outlook.com>
2015-12-19 22:35:39 -0500

    quick commit

--- src/delete.c
+++ src/delete.c
@@ -2,6 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 
-void delete(int document_id, int start_of_range, int end_of_range, int parent_id, int parent_machine_id){
-  
+void delete(char * document_id, int start_of_range, int end_of_range, int parent_id, int parent_machine_id){
+
 }
--- src/document.c
+++ src/document.c
@@ -1,13 +1,13 @@
 
 
-char * create_document(string document_contents){
-  
+char * create_document(char * document_contents){
+
 }
 
 void update_document(string updated_document_contents){
-  
+
 }
 
 void close_document(int document_id){
-  
+
 }
